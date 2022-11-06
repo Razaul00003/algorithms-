86c7fcba99fcca0ac5e24a07432d6d1ea77e1a77
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-11-06 06:23:06 +0600

    insertionSort

---
+++ InsertionSort.js
@@ -0,0 +1,14 @@
+//insertion sort
+function insertionSort() {
+  let temp;
+  for (let i = 1; i < array.length; i++) {
+    temp = array[i];
+    for (var j = i - 1; array[j] > temp && j > -1; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = temp;
+  }
+  return array;
+}
+
+console.log(insertionSort([12, 23, 42, 2, 7, 34, 3]));
